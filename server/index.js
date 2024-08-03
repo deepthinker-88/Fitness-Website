ca80f314e1826e15ea5caa9d07842f68bcd53498
James-Leigh Taylor <jamesleight@googlemail.com>
2024-08-03 18:46:08 +0100

    Added package-json files and express setup

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+dotenv.config();
+
+app.listen(PORT, () =>{
+    console.log(`Fitness app listening on port ${PORT}`)
+})
