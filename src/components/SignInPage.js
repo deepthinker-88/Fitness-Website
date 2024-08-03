16b649a6ce81f208df1171b6977874028315bcb7
James-Leigh Taylor <jamesleight@googlemail.com>
2024-08-03 23:42:41 +0100

    Reformatted code in SignInPage component

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -6,28 +6,30 @@ export default function SignIn() {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log("Email:",email, "Password:",password)
-    console.log("You have signed in")
-  }
-  return (<div>
-    <form onSubmit={handleLogin}>
+    console.log("Email:", email, "Password:", password);
+    console.log("You have signed in");
+  };
+  return (
+    
+    <div>
+      <form onSubmit={handleLogin}>
         <input
-            type="email"
-            placeholder="Please enter Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
+          type="email"
+          placeholder="Please enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
         />
 
         <input
-            type = "password"
-            placeholder = "Enter your password"
-            value ={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
-        <button type = "submit">Sign In</button>
-    </form>
-  </div>
+        <button className="sign-in-button-sign-in-page">Sign In</button>
+      </form>
+    </div>
   );
 }
