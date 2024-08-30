a43153bc6d30a80dab261a36753b478071557db3
James-Leigh Taylor <jamesleight@googlemail.com>
2024-08-30 18:10:44 +0100

    Formatted document for SignUpPage component

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,63 +1,72 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+export default function Signup() {
+  const navigate = useNavigate();
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default function Signup () {
-    const [firstname,setFirstName] = useState("");
-    const [lastname,setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password,setPassword] = useState("");
+  const handleSignUp = () => {
+    console.log("Signup email:", email, "password:", password);
+    console.log("You have signed up");
+  };
 
-    const handleSignUp = (e) => {
-        e.preventDefault();
-        console.log("Signup email:", email,"password:",password);
-        console.log("You have signed up")
-    };
+  const goToLoginPage = () => {
+    navigate("/sign-in");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSignUp}>
+        <input
+          type="text"
+          placeholder="Enter first name"
+          value={firstname}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
 
-    return (
-        <div>
-            <form onSubmit = {handleSignUp}> 
-                <input
-                    type ="text"
-                    placeholder = "Enter first name"
-                    value = {firstname}
-                    onChange={(e) => setFirstName(e.target.value)}
-                />
+        <input
+          type="text"
+          placeholder="Enter last name"
+          value={lastname}
+          onChange={(e) => setLastName(e.target.value)}
+        />
 
-                <input
-                    type = "text"
-                    placeholder = "Enter last name"
-                    value = {lastname}
-                    onChange = {(e) => setLastName(e.target.value)}
-                />
-           
-                <input
-                    type ="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
-                <input 
-                    type = "password"
-                    placeholder = "Enter password"
-                    value = {password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
 
-                />
+        <input
+          type="password"
+          placeholder="Enter confirm password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
 
-                <input
-                    type = "password"
-                    placeholder = "Enter confirm password"
-                    value = {password}
-                    onChange = {(e) => setPassword(e.target.value)}
-                    required
-                />
-
-                <button className="sign-up-button-sign-up-page"type = "submit" onClick = {handleSignUp}>Signup</button>
-
-            </form>
-        </div>
-    )
-};
-
+        <button
+          className="sign-up-button-sign-up-page"
+          type="submit"
+          onClick={() => {
+            handleSignUp();
+            goToLoginPage();
+          }}
+        >
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+}
