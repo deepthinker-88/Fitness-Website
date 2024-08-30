d949615327a8bf3f47842564557a297d16d9840a
James-Leigh Taylor <jamesleight@googlemail.com>
2024-08-30 18:08:07 +0100

    Removed browserrouter and routes from SignUpButton component

--- src/components/SignUpButton.js
+++ src/components/SignUpButton.js
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 
 export default function SignUpButton() {
