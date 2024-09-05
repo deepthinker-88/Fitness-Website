bcb1991b3da368b762f8d7ff16ffb68ec1e32416
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-05 22:14:54 +0100

    Added SignUpButton folder

---
+++ src/components/SignUpButton/SignUpButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SignUpButton() {
+  const navigate = useNavigate();
+
+  const goToSignUpPage = () => {
+    navigate("/sign-up");
+  };
+  return (
+    <button onClick={goToSignUpPage} className="sign-up-button">
+      Sign Up
+    </button>
+  );
+}
