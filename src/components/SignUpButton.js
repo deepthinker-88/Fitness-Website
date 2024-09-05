7041dd242001f4a96a01186538b35841a866f676
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-05 18:16:53 +0100

    Formatted document for SignUpButton component

--- src/components/SignUpButton.js
+++ src/components/SignUpButton.js
@@ -1,24 +1,15 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 export default function SignUpButton() {
   const navigate = useNavigate();
-  
-  const goToSignUpPage = () =>{
-    navigate("/sign-up");
-    
-  
-  }
-  ;
 
-  
-  
+  const goToSignUpPage = () => {
+    navigate("/sign-up");
+  };
   return (
-    <button onClick={goToSignUpPage}className="sign-up-button">
+    <button onClick={goToSignUpPage} className="sign-up-button">
       Sign Up
     </button>
-    
   );
-
 }
