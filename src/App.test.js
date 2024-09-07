4742b229a6a3c6ba8ef09abd945826d256c2948a
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-07 16:23:40 +0100

    Changed the name of the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Should show text Functional Fitness on homepage', () => {
+test('renders the landing page', () => {
   render(<App />);
   const textElements = screen.getAllByText(/Functional Fitness/i);
   const value = 'Functional Fitness';
