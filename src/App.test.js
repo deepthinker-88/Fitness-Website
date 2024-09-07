6902d0753d5161b54264fa0f226587e4dee86888
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-07 20:23:36 +0100

    Added more tests to test components rendering on the screen

--- src/App.test.js
+++ src/App.test.js
@@ -22,3 +22,24 @@ test('Sign In appears and is visible on the page', () => {
   expect(signInButton).toBeVisible();
   expect(signInButton).toBeInTheDocument();
 });
+
+test('Workout Programmes Button appears and is visible on the page',() => {
+  render(<App />)
+  const workoutProgrammesButton = screen.getByRole('button',{name:/workout programmes/i})
+  expect(workoutProgrammesButton).toBeVisible();
+  expect(workoutProgrammesButton).toBeInTheDocument();
+});
+
+test('Workouts Button appears and is visible on the page', () => {
+  render(<App />)
+  const workoutButton = screen.getByRole('button',{name:/workouts/i})
+  expect(workoutButton).toBeVisible();
+  expect(workoutButton).toBeInTheDocument();
+});
+
+test('Healthy Living Button appears and is visible on the page',()=>{
+  render(<App />)
+  const healthyLivingButton = screen.getByRole('button',{name:/healthy living/i})
+  expect(healthyLivingButton).toBeVisible();
+  expect(healthyLivingButton).toBeInTheDocument();
+});
