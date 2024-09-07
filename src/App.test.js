590adf8696666ce4110dfb55b920c684d02fb9b8
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-07 19:57:38 +0100

    Added tests to make sure individual button appear and are visible on my landing page

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,21 @@ test('renders the landing page', () => {
   const value = 'Functional Fitness';
   expect(textElements.some(el => el.textContent.includes(value))).toBe(true);
 });
+
+test('Sign Up appears and is visible on the same page',() => {
+  render(<App />)
+  const signUpButton = screen.getByRole('button',{name:/sign up/i})
+  expect(signUpButton).toBeVisible();
+  expect(signUpButton).toBeInTheDocument();
+});
+
+
+test('Sign In appears and is visible on the page', () => {
+  render(<App />)
+  const signInButton = screen.getByRole('button',{name:/sign in/i});
+  expect(signInButton).toBeVisible();
+  expect(signInButton).toBeInTheDocument();
+});
+
+
+
