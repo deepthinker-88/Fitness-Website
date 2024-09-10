d3f6c5aa822555beb5c1d59f1a8ed35de08c6841
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-10 22:56:55 +0100

    Changed name of component to WorkOut Button

--- src/components/WorkOutButton/WorkOutButton.js
+++ src/components/WorkOutButton/WorkOutButton.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function WorkOutButton() {
+  const navigate = useNavigate();
   const handleClick = () => {
     console.log("Workout button has been clicked");
   };
+
+  const goToWorkoutPage = () =>{
+    navigate('/workout-programmes')
+  }
 
   return (
     <div className="button-container">
---
+++ src/components/WorkOutButton/WorkOutButton.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import workoutButton from "./WorkOutButton";
+import { BrowserRouter } from "react-router-dom";
+
+test('Workout Button is visible on the screen',() => {
+    
+})
