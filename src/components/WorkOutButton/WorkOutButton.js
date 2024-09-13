baadc7ff8ba6b9a633cb8cdd01bbd69617a91b79
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-13 14:20:20 +0100

    Added Workout Button  function to navigate user to workout prgrammes page

--- src/components/WorkOutButton/WorkOutButton.js
+++ src/components/WorkOutButton/WorkOutButton.js
@@ -4,16 +4,21 @@ import { useNavigate } from "react-router-dom";
 export default function WorkOutButton() {
   const navigate = useNavigate();
   const handleClick = () => {
-    console.log("Workout button has been clicked");
   };
 
-  const goToWorkoutPage = () =>{
-    navigate('/workout-programmes')
-  }
+  const goToWorkoutsPage = () => {
+    navigate("/workouts");
+  };
 
   return (
-    <div className="button-container">
-      <button className="button-container" onClick={handleClick}>
+    <div>
+      <button
+        className="button-container"
+        onClick={() => {
+          goToWorkoutsPage();
+          handleClick();
+        }}
+      >
         Workouts
       </button>
     </div>
