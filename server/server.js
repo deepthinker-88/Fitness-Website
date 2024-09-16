ea9a8e64981d951337431edb5ff6d5dc0125f529
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-16 22:57:21 +0100

    Added confirm password validator for sending user data to the backend

--- server/server.js
+++ server/server.js
@@ -1,40 +1,50 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import User from '../model/User.js';
-import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import express from "express";
+import User from "../model/User.js";
+import mongoose from "mongoose";
+import cors from "cors";
 dotenv.config();
-
 const app = express();
+app.use(cors({ origin: true, credentials: true }));
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('Connected to MongoDB'))
-.catch((err)=> console.log(`MongoDB Connection Error: ${err}`))
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(`MongoDB Connection Error: ${err}`));
 
-app.get('/users', async (req,res) => {
-    try{
-        const users = await User.find({})
-        res.status(200).json(users);
-    } catch(error) {
-        console.log(`Can't find users ${error}`);
-    };
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(`Can't find users ${error}`);
+  }
 });
 
-
-app.post('/users', async (req, res) => {
-    try {
-        const {firstname, lastname,Email,Password} = req.body;
-        const newUser = new User({firstname,lastname,Email,Password});
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser)
-    } catch (error) {
-        res.status(500).send(`There has been an error saving the saved user ${error}`);
-    }
+app.post("/users", async (req, res) => {
+  try {
+    const { firstname, lastname, email, password, confirmPassword } = req.body;
+    const newUser = new User({
+      firstname,
+      lastname,
+      email,
+      password,
+      confirmPassword,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res
+      .status(500)
+      .send(`There has been an error saving the saved user ${error}`);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Fitness app listening on port ${PORT}`);
+  console.log(`Fitness app listening on port ${PORT}`);
 });
 console.log(process.env.MONGODB_URI);
