15e3bce9693e4beee91b0cff41daf46a72dd9171
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-17 17:10:25 +0100

    Refactored code

--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import BackToHomePageButton from "../BackToHomePageButton/BackToHomePageButton";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export default function Signup() {
   const navigate = useNavigate();
@@ -8,17 +10,36 @@ export default function Signup() {
   const [lastname, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState(null);
 
-  
-  const handleSignUp = () => {
-    console.log("Signup email:", email, "password:", password);
-    console.log("You have signed up");
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+    try {
+      const userDetails = {
+        firstname,
+        lastname,
+        email,
+        password,
+        confirmPassword,
+      };
+
+      const response = await axios.post(apiUrl, userDetails);
+      if (response.status === 201) {
+        alert("You have successfully signed up");
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      console.log(
+        `Error saving user details to database: ${error.response.data}`
+      );
+    }
   };
 
-  
-  const goToLoginPage = () => {
-    navigate("/sign-in");
-  };
   return (
     <div>
       <form onSubmit={handleSignUp}>
@@ -28,7 +49,6 @@ export default function Signup() {
           value={firstname}
           onChange={(e) => setFirstName(e.target.value)}
         />
-
         <input
           type="text"
           placeholder="Enter last name"
@@ -42,32 +62,26 @@ export default function Signup() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
+          autoComplete="username"
         />
-
         <input
           type="password"
           placeholder="Enter password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
+          autoComplete="password"
         />
-
         <input
           type="password"
           placeholder="Confirm password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
           required
+          autoComplete="new-password"
         />
-
-        <button
-          className="sign-up-button-sign-up-page"
-          type="submit"
-          onClick={() => {
-            handleSignUp();
-            goToLoginPage();
-          }}
-        >
+        error && <p style={{ color: "red" }}>{error}</p>
+        <button className="sign-up-button-sign-up-page" type="submit">
           Signup
         </button>
       </form>
