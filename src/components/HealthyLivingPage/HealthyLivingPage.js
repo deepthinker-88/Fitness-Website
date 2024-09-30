05742f28319837d40160b1a630cedf5e1e5bd840
James-Leigh Taylor <jamesleight@googlemail.com>
2024-09-30 22:20:11 +0100

    Added font for Healthy Living Page

--- src/components/HealthyLivingPage/HealthyLivingPage.js
+++ src/components/HealthyLivingPage/HealthyLivingPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from "react";
 
-export default function HealthyLivingPage() {
-    
+export default function HealthyLivingPage(props) {
+  return <h1>{props.title}</h1>;
 }
