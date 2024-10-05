76d51b69a04967686ea27c88a215b956b41c6430
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-05 13:23:10 +0100

    Added Functional Fitness text to be included in center on main page

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                 <Navbar />
 
                 <img className="image-dimensions" src={fitness} alt="fitness" />
-                <h1 className="center-title"> Functional Fitness</h1>
+                <h1 className="text-colour"> Functional Fitness</h1>
                 <h2 className="text-colour">
                   Welcome to The Functional Fitness website. Let's get started
                   to get you in shape
