ca2b940f0645b6e5aa0544a3055bca4eaa488958
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-07 21:22:07 +0100

    Added CSS classname for images for healthy living page component

--- src/components/HealthyLivingPage/HealthyLivingPage.js
+++ src/components/HealthyLivingPage/HealthyLivingPage.js
@@ -1,11 +1,29 @@
 import React from "react";
+import NavBar from "../NavBar/NavBar";
+import healthySalmonPasta from "../../images/FoodImages/healthySalmonPasta.png";
+import chickenStirFry from "../../images/FoodImages/healthyChickenStirFry.png";
+import burritoBowl from "../../images/FoodImages/burritoBowl.png"
+import BackToHomePageButton from "../BackToHomePageButton/BackToHomePageButton";
+import "./HealthyLivingPage.css"
 
 export default function HealthyLivingPage(props) {
 
   return (
   <div>
+    <NavBar/>
+    <BackToHomePageButton className="back-button-healthy-living-page" />
+    
   <h1>{props.name}</h1>
+  <img className="healthy-images" src={healthySalmonPasta} alt ="salmon food"/>
+  <img className="healthy-images"  src = {chickenStirFry} alt = "chicken food"/>
+  <img className="healthy-images"  src={burritoBowl} alt = "burrito bowl food"/>
+  
   <h1>{props.title}</h1>
+  <ul>
+    <li><strong>Consume less calories than you eat</strong></li>
+    <li><strong>Exercise three times a week</strong></li>
+    <li><strong>Look after your mental health</strong></li>
+  </ul>
   </div>
   )}
 
