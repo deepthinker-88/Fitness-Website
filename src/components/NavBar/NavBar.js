838746456bc9e1f79337def0e79102824d8fcf11
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-13 13:33:11 +0100

    Added Navbar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+
+export default function NavBar(){
+
+  return(
+    <nav className='navbar'>
+      <ul>
+      <li><a href="/sign-up">Sign Up</a></li>
+      <li><a href="/sign-in">Sign In</a></li>
+      <li><a href="/workout-programmes">Workout Programmes</a></li>
+      <li><a href="/workouts">Workouts</a></li>
+      <li><a href="/healthy-living">Healthy Living</a></li>
+
+      </ul>
+      </nav>
+      )}
+
