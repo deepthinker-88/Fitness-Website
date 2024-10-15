c9edf6543891b7e87ee0fe7d712f203da60c1265
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-15 22:23:10 +0100

    Added UserProfilePage css and styling for UserProfilePage Component

--- src/components/UserProfilePage/UserProfilePage.js
+++ src/components/UserProfilePage/UserProfilePage.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./UserProfilePage.css";
+import BackToHomePageButton from "../BackToHomePageButton/BackToHomePageButton";
 
-export default function UserProfile(){
-    
+export default function UserProfile(props) {
+  return (
+    <div>
+      <header className="navbar-user-profile-page">
+        <BackToHomePageButton />
+      </header>
+
+      <h1>{props.title}</h1>
+    </div>
+  );
 }
