9346966ed66b9d04c021852eeda842f0e58641a7
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-15 21:43:04 +0100

    Added Footer to WorkOuts Page

--- src/components/WorkOutsPage/WorkOutsPage.js
+++ src/components/WorkOutsPage/WorkOutsPage.js
@@ -1,5 +1,19 @@
 import React from 'react';
-
-export default function WorkOutsPage(){
+import './WorkOutsPage.css'
+import BackToHomePageButton from '../BackToHomePageButton/BackToHomePageButton';
+import Footer from '../Footer/Footer';
+export default function WorkOutsPage(props){
+    return(
+        <div>
+            <header className='navbar-workouts-page'>
+            <BackToHomePageButton/>
+            </header>
+        <h1 className="workouts-text">{props.title}</h1>
+        <h2>Upper Body Workout</h2>
+        <h2>Lower Body Workout</h2>
+        <h2>Full-Body Workout</h2>
+        <Footer />
+        </div>
+    )
 
 }
