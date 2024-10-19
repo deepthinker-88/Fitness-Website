a7a77309e78188d6780b415eb8d56377960f5e6f
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-19 12:08:09 +0100

    Updated page and css for Healthy Living Page

--- src/components/HealthyLivingPage/HealthyLivingPage.js
+++ src/components/HealthyLivingPage/HealthyLivingPage.js
@@ -1,28 +1,32 @@
 import React from "react";
-import NavBar from "../NavBar/NavBar";
+import './HealthyLivingPage.css';
 import healthySalmonPasta from "../../images/FoodImages/healthySalmonPasta.png";
 import chickenStirFry from "../../images/FoodImages/healthyChickenStirFry.png";
 import burritoBowl from "../../images/FoodImages/burritoBowl.png"
 import BackToHomePageButton from "../BackToHomePageButton/BackToHomePageButton";
-import "./HealthyLivingPage.css"
+import "./HealthyLivingPage.css";
 
 export default function HealthyLivingPage(props) {
 
   return (
-  <div>
-    <NavBar/>
-    <BackToHomePageButton className="back-button-healthy-living-page" />
+  <div className="content">
+    <header className="navbar-healthy-living-page">
+    <BackToHomePageButton />
+    </header>
     
-  <h1>{props.name}</h1>
+  <h1 class = 'font'>{props.name}</h1>
   <img className="healthy-images" src={healthySalmonPasta} alt ="salmon food"/>
   <img className="healthy-images"  src = {chickenStirFry} alt = "chicken food"/>
   <img className="healthy-images"  src={burritoBowl} alt = "burrito bowl food"/>
   
-  <h1>{props.title}</h1>
-  <ul>
+  <h1 class="font">{props.title}</h1>
+  <ul class="font">
     <li><strong>Consume less calories than you eat</strong></li>
+    <br></br>
     <li><strong>Exercise three times a week</strong></li>
+    <br></br>
     <li><strong>Look after your mental health</strong></li>
+    <br></br>
   </ul>
   </div>
   )}
