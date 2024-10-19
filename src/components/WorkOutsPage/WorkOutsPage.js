3357c8f48f41e9e1db04a7db943dbe5337c1511a
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-19 12:07:04 +0100

    Updated page and css for WorkOuts Page

--- src/components/WorkOutsPage/WorkOutsPage.js
+++ src/components/WorkOutsPage/WorkOutsPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './WorkOutsPage.css'
 import BackToHomePageButton from '../BackToHomePageButton/BackToHomePageButton';
-import Footer from '../Footer/Footer';
 export default function WorkOutsPage(props){
     return(
-        <div>
+        <div className='content'>
             <header className='navbar-workouts-page'>
             <BackToHomePageButton/>
             </header>
@@ -12,7 +11,6 @@ export default function WorkOutsPage(props){
         <h2>Upper Body Workout</h2>
         <h2>Lower Body Workout</h2>
         <h2>Full-Body Workout</h2>
-        <Footer />
         </div>
     )
 
