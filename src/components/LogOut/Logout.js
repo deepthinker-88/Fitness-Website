2344c6e4803783cc3f74c56a7d6a4620e751df76
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-23 00:28:41 +0100

    Added logout functionality for userprofile page

---
+++ src/components/LogOut/Logout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Logout.css";
+
+export default function LogoutToHomePageButton() {
+  return (
+    <li className="logout">
+      <a href="/">logout</a>
+    </li>
+  );
+}
