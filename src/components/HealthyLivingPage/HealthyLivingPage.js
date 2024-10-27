77db6c2014012e37c40b53938e2f5a5822475034
James-Leigh Taylor <jamesleight@googlemail.com>
2024-10-27 11:05:38 +0000

    Updated logic for HealthyLivingPage

--- src/components/HealthyLivingPage/HealthyLivingPage.js
+++ src/components/HealthyLivingPage/HealthyLivingPage.js
@@ -1,34 +1,41 @@
 import React from "react";
-import './HealthyLivingPage.css';
+import "./HealthyLivingPage.css";
 import healthySalmonPasta from "../../images/FoodImages/healthySalmonPasta.png";
 import chickenStirFry from "../../images/FoodImages/healthyChickenStirFry.png";
-import burritoBowl from "../../images/FoodImages/burritoBowl.png"
+import burritoBowl from "../../images/FoodImages/burritoBowl.png";
 import BackToHomePageButton from "../BackToHomePageButton/BackToHomePageButton";
 import "./HealthyLivingPage.css";
 
 export default function HealthyLivingPage(props) {
-
-  return (
-  <div className="content">
-    <header className="navbar-healthy-living-page">
-    <BackToHomePageButton />
-    </header>
-    
-  <h1 class = 'font'>{props.name}</h1>
-  <img className="healthy-images" src={healthySalmonPasta} alt ="salmon food"/>
-  <img className="healthy-images"  src = {chickenStirFry} alt = "chicken food"/>
-  <img className="healthy-images"  src={burritoBowl} alt = "burrito bowl food"/>
+  const images = [healthySalmonPasta, chickenStirFry, burritoBowl];
+  const recipes=['Salmon Pasta','Chicken Stir Fry','Burrito Bowl']
   
-  <h1 class="font">{props.title}</h1>
-  <ul class="font">
-    <li><strong>Consume less calories than you eat</strong></li>
-    <br></br>
-    <li><strong>Exercise three times a week</strong></li>
-    <br></br>
-    <li><strong>Look after your mental health</strong></li>
-    <br></br>
-  </ul>
-  </div>
-  )}
+  return (
+    <div className="content">
+      <header className="navbar-healthy-living-page">
+        <BackToHomePageButton />
+      </header>
+      <h1 className="font">{props.title}</h1>
 
+      <ul>
+        {images.map((item,index) => (
+          <li>
+            <img src={item} alt="Food Items" />
+            <p className="font">{recipes[index]}</p> 
+           
+          </li>
+        ))}
+      </ul>
+      <br></br>
+      <h2 className="font">Maintaing a healthy lifestyle</h2>
+      <br></br>
+      <p className='font'>Consume less calories than you eat</p>
+      <br></br>
+      <p className="font">Exercise three times a week</p>
+      <br></br>
+      <p className="font">Look after your mental health</p>
+
+    </div>
+  );
+}
 
