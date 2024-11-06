fed03d895ee97de02fef4e6eedbba74d12ee671c
James-Leigh Taylor <jamesleight@googlemail.com>
2024-11-06 11:00:06 +0000

    Updated tests for landing page

--- src/App.test.js
+++ src/App.test.js
@@ -8,38 +8,38 @@ test('renders the landing page', () => {
   expect(textElements.some(el => el.textContent.includes(value))).toBe(true);
 });
 
-test('Sign Up Button appears and is visible on the same page',() => {
+test('Sign Up Link appears and is visible on the same page',() => {
   render(<App />)
-  const signUpButton = screen.getByRole('button',{name:/sign up/i})
-  expect(signUpButton).toBeVisible();
-  expect(signUpButton).toBeInTheDocument();
+  const signUpLink = screen.getByRole('link',{name:/sign up/i})
+  expect(signUpLink).toBeVisible();
+  expect(signUpLink).toBeInTheDocument();
 });
 
 
-test('Sign In Button appears and is visible on the page', () => {
+test('Sign In Link appears and is visible on the page', () => {
   render(<App />)
-  const signInButton = screen.getByRole('button',{name:/sign in/i});
-  expect(signInButton).toBeVisible();
-  expect(signInButton).toBeInTheDocument();
+  const signInLink = screen.getByRole('link',{name:/sign in/i});
+  expect(signInLink).toBeVisible();
+  expect(signInLink).toBeInTheDocument();
 });
 
-test('Workout Programmes Button appears and is visible on the page',() => {
+test('Workout Programmes Link and is visible on the page',() => {
   render(<App />)
-  const workoutProgrammesButton = screen.getByRole('button',{name:/workout programmes/i})
-  expect(workoutProgrammesButton).toBeVisible();
-  expect(workoutProgrammesButton).toBeInTheDocument();
+  const workoutProgrammesLink = screen.getByRole('link',{name:/workout programmes/i})
+  expect(workoutProgrammesLink).toBeVisible();
+  expect(workoutProgrammesLink).toBeInTheDocument();
 });
 
-test('Workouts Button appears and is visible on the page', () => {
+test('Workouts Link appears and is visible on the page', () => {
   render(<App />)
-  const workoutButton = screen.getByRole('button',{name:/workouts/i})
-  expect(workoutButton).toBeVisible();
-  expect(workoutButton).toBeInTheDocument();
+  const workoutsLink = screen.getByRole('link',{name:/workouts/i})
+  expect(workoutsLink).toBeVisible();
+  expect(workoutsLink).toBeInTheDocument();
 });
 
-test('Healthy Living Button appears and is visible on the page',()=>{
+test('Healthy Living Link appears and is visible on the page',()=>{
   render(<App />)
-  const healthyLivingButton = screen.getByRole('button',{name:/healthy living/i})
-  expect(healthyLivingButton).toBeVisible();
-  expect(healthyLivingButton).toBeInTheDocument();
+  const healthyLivingLink = screen.getByRole('link',{name:/healthy living/i})
+  expect(healthyLivingLink).toBeVisible();
+  expect(healthyLivingLink).toBeInTheDocument();
 });
