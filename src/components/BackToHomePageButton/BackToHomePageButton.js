cb1d8338182d0bf2270e448354dda6b41d9c75c2
James-Leigh Taylor <jamesleight@googlemail.com>
2024-11-07 15:35:31 +0000

    Updated BackToHomePage Button component

--- src/components/BackToHomePageButton/BackToHomePageButton.js
+++ src/components/BackToHomePageButton/BackToHomePageButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./BackToHomePageButton.css";
+import { useNavigate } from "react-router-dom";
 
 export default function BackToHomePageButton() {
+  const navigate = useNavigate();
+  const handleGoBackToHomePageLink = () => {
+    navigate("/");
+  };
   return (
-    <li className="back-to-homepage">
+    <li className="back-to-homepage" onClick={handleGoBackToHomePageLink}>
       <a href="/">back to homepage</a>{" "}
     </li>
   );
