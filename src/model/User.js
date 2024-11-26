172be07d8734efc24ac6f0285a9114516da4aa8f
James-Leigh Taylor <jamesleight@googlemail.com>
2024-11-26 22:10:41 +0000

    Removed confirmpassword logic

--- src/model/User.js
+++ src/model/User.js
@@ -1,32 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstname:{
-        type: String,
-        required:true
-    },
+  firstname: {
+    type: String,
+    required: true,
+  },
 
-    lastname:{
-        type: String,
-        required:true
-    },
+  lastname: {
+    type: String,
+    required: true,
+  },
 
-    email:{
-        type: String,
-        required:true
-    },
+  email: {
+    type: String,
+    required: true,
+  },
 
-    password:{
-        type: String,
-        required:true
-    },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-    confirmPassword:{
-        type:String,
-        required:true
-    }
-})
-
-
-module.exports = mongoose.model('User',userSchema)
+module.exports = mongoose.model("User", userSchema);
