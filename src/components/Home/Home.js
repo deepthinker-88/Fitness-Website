e59da107026184450921dca0435135f000ac291a
James-Leigh Taylor <jamesleight@googlemail.com>
2025-03-16 01:24:14 +0000

    Added HomePage Component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <main className="main-page">
+      <h1 className="home-page--title">
+        Welcome to Functional Fitness
+        <br />
+        Let's get started to get you in shape
+      </h1>
+    </main>
+  );
+}
