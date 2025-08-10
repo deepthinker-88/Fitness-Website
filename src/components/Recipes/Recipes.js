142f1116328d76a3fc0bf765db748164d7365eca
James-Leigh Taylor <jamesleight@googlemail.com>
2025-08-10 21:09:47 +0100

    Added functionality to change button text

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,8 +1,11 @@
 import "./Recipes.css";
+import Footer from "../Footer/Footer";
 import React from "react";
 import { useState } from "react";
 export default function Recipes() {
   const [recipes, setRecipes] = useState("");
+  const [showSaveButton, setShowSaveButton] = useState(false);
+  const [showSavedRecipes, setShowSavedRecipes] = useState(false);
 
   const handleFindRecipe = async () => {
     const url = fetch("https://www.themealdb.com/api/json/v1/1/random.php");
@@ -10,9 +13,15 @@ export default function Recipes() {
     const response = await url;
     const json = await response.json();
     setRecipes(json.meals);
+    setShowSaveButton(true);
   };
+
+  const handleSavedRecipes = async () => {
+    setShowSavedRecipes(true);
+  };
+
   return (
-    <section>
+    <section className="recipe-info">
       <h2>Let's start finding some great recipes for you</h2>
       <p>Recipe Finder</p>
       <button onClick={handleFindRecipe}>
@@ -23,23 +32,35 @@ export default function Recipes() {
       <br></br>
 
       {recipes && recipes.length > 0 ? (
-        <ul>
+        <section className="recipe-instructions">
           {recipes.map((recipe) => {
             return (
-              <div key={recipe.idMeal}>
-                <h1>{recipe.strMeal}</h1>
-                <p>{recipe.strInstructions}</p>
+              <div key={recipe.idMeal} class="food-list">
+                <br></br>
                 <img
                   src={recipe.strMealThumb}
                   alt="food-image"
                   width="150"
+                  class="responsive"
                 ></img>
+                <h1>{recipe.strMeal}</h1>
+                <p>{recipe.strInstructions}</p>
+                <p className="youtube-link">
+                  {" "}
+                  YouTube Link : {recipe.strYoutube}
+                </p>
               </div>
             );
           })}
-        </ul>
+        </section>
       ) : (
         <p>No recipes found</p>
+      )}
+
+      {showSaveButton && (
+        <button onClick={handleSavedRecipes}>
+          {showSavedRecipes ? "Recipe Saved" : "Save Recipe"}
+        </button>
       )}
     </section>
   );
