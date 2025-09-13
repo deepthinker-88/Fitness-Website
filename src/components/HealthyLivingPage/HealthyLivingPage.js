92ca9c3fabd5367094069fb8644788f49c47d3f9
James-Leigh Taylor <jamesleight@googlemail.com>
2025-09-13 11:52:23 +0100

    first draft of HealthyLivingPage with flip card logic

--- src/components/HealthyLivingPage/HealthyLivingPage.js
+++ src/components/HealthyLivingPage/HealthyLivingPage.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import "./HealthyLivingPage.css";
 import healthySalmonPasta from "../../images/FoodImages/healthySalmonPasta.png";
 import chickenStirFry from "../../images/FoodImages/healthyChickenStirFry.png";
 import burritoBowl from "../../images/FoodImages/burritoBowl.png";
 import smileyFace from "../../images/HealthyMindsetImages/smileyFace.png";
 import mindfulImage from "../../images/HealthyMindsetImages/mindful.png";
-import sleep from "../../images/HealthyMindsetImages/sleep.png"
+import sleep from "../../images/HealthyMindsetImages/sleep.png";
 import BackToHomePageButton from "../BackToHomePageButton/BackToHomePageButton";
 import "./HealthyLivingPage.css";
 import Footer from "../Footer/Footer";
@@ -13,6 +14,45 @@ import Footer from "../Footer/Footer";
 export default function HealthyLivingPage({ title }) {
   const images = [healthySalmonPasta, chickenStirFry, burritoBowl];
   const recipes = ["Salmon Pasta", "Chicken Stir Fry", "Burrito Bowl"];
+  const [emojiIsFlipped, setEmojiIsFlipped] = useState(true);
+  const [emojiCardChange, setEmojiCardChange] = useState(false);
+  const [mindfulIsFlipped, setMindfulIsFlipped] = useState(true);
+  const [mindfulCardChange, setMindfulCardChange] = useState(false);
+  const [sleepIsFlipped, setSleepIsFlipped] = useState(true);
+  const [sleepCardChange, setSleepCardChange] = useState(false);
+
+  const handleSmileyChange = () => {
+    setTimeout(() => {
+      setEmojiIsFlipped(false);
+      setEmojiCardChange(true);
+    }, 2000);
+    setTimeout(() => {
+      setEmojiIsFlipped(true);
+      setEmojiCardChange(false);
+    }, 4000);
+  };
+
+  const handleMindfulChange = () => {
+    setTimeout(() => {
+      setMindfulIsFlipped(false);
+      setMindfulCardChange(true);
+    }, 2000);
+    setTimeout(() => {
+      setMindfulIsFlipped(true);
+      setMindfulCardChange(false);
+    }, 4000);
+  };
+
+  const handleSleepChange = () => {
+    setTimeout(() => {
+      setSleepIsFlipped(false);
+      setSleepCardChange(true);
+    }, 2000);
+    setTimeout(() => {
+      setSleepIsFlipped(true);
+      setSleepCardChange(false);
+    }, 4000);
+  };
 
   return (
     <div className="healthy-overview">
@@ -42,9 +82,50 @@ export default function HealthyLivingPage({ title }) {
       <section className="healthy-mindset">
         <h2>Maintaining a healthy Mindset</h2>
         <section className="mental-health-images">
-          <img src={smileyFace} alt="smileyImage" height="150" />
-          <img src={mindfulImage} alt="mindfulImage" height="150"/>
-           <img src={sleep} alt="sleepImage" height="150"/>
+          {emojiIsFlipped && (
+            <img
+              onClick={handleSmileyChange}
+              className="image1"
+              src={smileyFace}
+              alt="smileyImage"
+              height="250"
+            />
+          )}
+          {emojiCardChange && (
+            <>
+              <section className="health-text">
+                <p>Looking after Your Mental Health is key</p>
+                <p>Having a Growth Mindset is important</p>
+                <p>Think Positively</p>
+              </section>
+            </>
+          )}
+
+          {mindfulIsFlipped && (
+            <>
+              <img
+                onClick={handleMindfulChange}
+                className="image2"
+                src={mindfulImage}
+                alt="mindfulImage"
+                height="250"
+              />
+            </>
+          )}
+
+          {mindfulCardChange && <p>Stay in the present moment</p>}
+
+          {sleepIsFlipped && (
+            <img
+              onClick={handleSleepChange}
+              className="image3"
+              src={sleep}
+              alt="sleepImage"
+              height="180"
+            />
+          )}
+
+          {sleepCardChange && <p>Get 7-9 hours of sleep per night</p>}
         </section>
       </section>
       <section className="healthy-living">
