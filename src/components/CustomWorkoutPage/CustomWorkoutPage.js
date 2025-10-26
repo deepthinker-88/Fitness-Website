cd91096a14bb69e651f8753779627cd93cd09431
James-Leigh Taylor <jamesleight@googlemail.com>
2025-10-26 19:08:18 +0000

    Added css file for CustomWorkoutPage component

---
+++ src/components/CustomWorkoutPage/CustomWorkoutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import "./CustomWorkoutPage.css"
+
+export default function ShowCustomWorkouts (props) {
+    return(
+    <div className="custom-workout-info">
+        <h1 className="custom-workout-title">Please find your custom Workouts Below</h1>
+
+        
+    </div>
+    )
+
+}
